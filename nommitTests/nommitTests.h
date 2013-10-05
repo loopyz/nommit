978f429d67c24e8abc5d07ed4eff27963f767e99
Gregory Rose <greg@leanplum.com>
2013-10-05 14:35:15 -0400

    other crap

---
+++ nommitTests/nommitTests.h
@@ -0,0 +1,13 @@
+//
+//  nommitTests.h
+//  nommitTests
+//
+//  Created by Gregory Rose on 9/30/13.
+//  Copyright (c) 2013 Gregory Rose. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface nommitTests : SenTestCase
+
+@end
---
+++ nommitTests/nommitTests.m
@@ -0,0 +1,32 @@
+//
+//  nommitTests.m
+//  nommitTests
+//
+//  Created by Gregory Rose on 9/30/13.
+//  Copyright (c) 2013 Gregory Rose. All rights reserved.
+//
+
+#import "nommitTests.h"
+
+@implementation nommitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in nommitTests");
+}
+
+@end
